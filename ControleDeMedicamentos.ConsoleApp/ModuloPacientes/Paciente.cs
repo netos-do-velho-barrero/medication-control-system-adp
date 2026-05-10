5f1608c96999dadfe2b63ec4eabdeb4c9a7b5c94
Marco-Oliver <marcooliver2@yahoo.com>
2026-05-10 14:46:21 -0300

    feat: implement patient class

--- ControleDeMedicamentos.ConsoleApp/ModuloPacientes/Paciente.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloPacientes/Paciente.cs
@@ -1,4 +1,4 @@
-using System;
+
 using ControleDeMedicamentos.ConsoleApp.Compartilhado;
 
 namespace ControleDeMedicamentos.ConsoleApp.ModuloPacientes;
@@ -9,13 +9,55 @@ public class Paciente : EntidadeBase
 
   public string Telefone { get; set; }
 
-  public override void AtualizarDados(EntidadeBase entidadeAtualizada)
+  public string CartaoSus { get; set; }
+
+  public string Cpf { get; set; }
+
+  public Paciente()
   {
-    throw new NotImplementedException();
+  }
+
+  public Paciente(
+    string nome,
+    string telefone,
+    string cartaoSus,
+    string cpf
+   )
+  {
+    Nome = nome;
+    Telefone = telefone;
+    CartaoSus = cartaoSus;
+    Cpf = cpf;
   }
 
   public override List<string> Validar()
   {
-    throw new NotImplementedException();
+    List<string> erros = new List<string>();
+
+    if (Nome.Length < 3 || Nome.Length > 100)
+      erros.Add("O campo \"Nome\" deve conter entre 2 e 100 caracteres.");
+
+    if (Telefone.Length < 14 || Telefone.Length > 15)
+      erros.Add("O campo \"Telefone\" deve ser preenchido corretamente: (XX) XXXX-XXXX");
+
+    if (CartaoSus.Length != 15)
+      erros.Add("O campo \"CartaoSus\" deve conter 15 digitos.");
+
+    if (Cpf.Length != 11)
+      erros.Add("O campo \"Cpf\" deve conter 11 digitos.");
+
+    return erros;
   }
+
+
+  public override void AtualizarDados(EntidadeBase entidadeAtualizada)
+  {
+      Paciente pacienteAtualizado = (Paciente)entidadeAtualizada;
+
+      Nome = pacienteAtualizado.Nome;
+      Telefone = pacienteAtualizado.Telefone;
+      CartaoSus = pacienteAtualizado.CartaoSus;
+      Cpf = pacienteAtualizado.Cpf;
+  }
+
 }
