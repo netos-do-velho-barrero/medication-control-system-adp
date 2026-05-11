4cdc14fd39168078bb8605639dbc160e348d2235
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-05-11 15:11:15 -0300

    fix: resolve basic error in TelaFuncionario

--- ControleDeMedicamentos.ConsoleApp/ModuloFuncionarios/TelaFuncionario.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloFuncionarios/TelaFuncionario.cs
@@ -1,51 +1,51 @@
 using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.ModuloFornecedores;
 using ControleDeMedicamentos.ConsoleApp.Utilidades;
 
+namespace ControleDeMedicamentos.ConsoleApp.ModuloFuncionarios;
 
-namespace ControleDeMedicamentos.ConsoleApp.ModuloFornecedores;
-
-public class TelaFuncionario : TelaBase<Fornecedor>, ITelaOpcoes, ITelaCrud
+public class TelaFuncionario : TelaBase<Funcionario>, ITelaOpcoes, ITelaCrud
 {
-  public TelaFuncionario(IRepositorio<Fornecedor> repositorio) : base("Funcionario", repositorio)
-  {
-  }
-
-  public override void VisualizarTodos(bool deveExibirCabecalho)
-  {
-    if (deveExibirCabecalho)
-      ExibirCabecalho("Visualização de Pacientes");
-
-    List<Funcionario> funcionarios = repositorio.SelecionarTodos(); //repositorio.SelecionarTodos();
-
-    if (funcionarios.Count == 0)
+    public TelaFuncionario(IRepositorio<Funcionario> repositorio) : base("Funcionario", repositorio)
     {
-      Notificador.ExibirMensagem("Nenhum item registrado.");
-      return;
     }
 
-    Console.WriteLine(
-        "{0, -7} | {1, -10} | {2, -15} | {3, -20} | {4, -15}",
-        "Id", "Nome", "Telefone", "Cartao do Sus", "CPF"
-    );
-
-    foreach (Funcionario p in funcionarios)
+    public override void VisualizarTodos(bool deveExibirCabecalho)
     {
-      Console.WriteLine(
-          "{0, -7} | {1, -10} |  {3, -20} | {4, -15}",
-          p.Id, p.Nome, p.Telefone, p.Cpf
-      );
+        if (deveExibirCabecalho)
+            ExibirCabecalho("Visualização de Pacientes");
+
+        List<Funcionario> funcionarios = repositorio.SelecionarTodos(); //repositorio.SelecionarTodos();
+
+        if (funcionarios.Count == 0)
+        {
+            Notificador.ExibirMensagem("Nenhum item registrado.");
+            return;
+        }
+
+        Console.WriteLine(
+            "{0, -7} | {1, -10} | {2, -15} | {3, -20} | {4, -15}",
+            "Id", "Nome", "Telefone", "Cartao do Sus", "CPF"
+        );
+
+        foreach (Funcionario f in funcionarios)
+        {
+            Console.WriteLine(
+                "{0, -7} | {1, -10} |  {3, -20} | {4, -15}",
+                f.Id, f.Nome, f.Telefone, f.Cpf
+            );
+        }
+
+        if (deveExibirCabecalho)
+        {
+            Console.WriteLine("---------------------------------");
+            Console.Write("Digite ENTER para continuar...");
+            Console.ReadLine();
+        }
     }
 
-    if (deveExibirCabecalho)
+    protected override Funcionario ObterDadosCadastrais()
     {
-      Console.WriteLine("---------------------------------");
-      Console.Write("Digite ENTER para continuar...");
-      Console.ReadLine();
+        throw new NotImplementedException();
     }
-  }
-
-  protected override Fornecedor ObterDadosCadastrais()
-  {
-    throw new NotImplementedException();
-  }
 }
