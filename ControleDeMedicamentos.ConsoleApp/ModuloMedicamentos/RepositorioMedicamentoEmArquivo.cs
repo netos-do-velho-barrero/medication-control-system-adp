70ada2ed1f58441b250f09b4c42d81331942ac1b
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-05-11 01:14:34 -0300

    feat: implement RepositorioMedicamentoEmArquivo repository

---
+++ ControleDeMedicamentos.ConsoleApp/ModuloMedicamentos/RepositorioMedicamentoEmArquivo.cs
@@ -0,0 +1,16 @@
+using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.Compartilhado.Arquivos;
+
+namespace ControleDeMedicamentos.ConsoleApp.ModuloMedicamentos;
+
+public class RepositorioMedicamentoEmArquivo : RepositorioBaseEmArquivo<Medicamento>, IRepositorio<Medicamento>
+{
+    public RepositorioMedicamentoEmArquivo(ContextoJson contexto) : base(contexto)
+    {
+    }
+
+    protected override List<Medicamento> CarregarRegistros()
+    {
+        return contexto.Medicamentos;
+    }
+}
