cf58c1c1070c78f3fb29b79a95ca46dd63ac6b75
Marco-Oliver <marcooliver2@yahoo.com>
2026-05-11 01:47:47 -0300

    feat(employee): implement employee screen and add new option to main menu

--- ControleDeMedicamentos.ConsoleApp/ModuloFuncionarios/TelaFuncionario.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloFuncionarios/TelaFuncionario.cs
@@ -1,51 +1,80 @@
 using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.ModuloFornecedores;
 using ControleDeMedicamentos.ConsoleApp.Utilidades;
 
 
-namespace ControleDeMedicamentos.ConsoleApp.ModuloFornecedores;
-
-public class TelaFuncionario : TelaBase<Fornecedor>, ITelaOpcoes, ITelaCrud
+namespace ControleDeMedicamentos.ConsoleApp.ModuloFuncionarios
 {
-  public TelaFuncionario(IRepositorio<Fornecedor> repositorio) : base("Funcionario", repositorio)
+  public class TelaFuncionario : TelaBase<Funcionario>, ITelaOpcoes, ITelaCrud
   {
-  }
-
-  public override void VisualizarTodos(bool deveExibirCabecalho)
-  {
-    if (deveExibirCabecalho)
-      ExibirCabecalho("Visualização de Pacientes");
-
-    List<Funcionario> funcionarios = repositorio.SelecionarTodos(); //repositorio.SelecionarTodos();
-
-    if (funcionarios.Count == 0)
+    public TelaFuncionario(IRepositorio<Funcionario> repositorio) : base("Funcionario", repositorio)
     {
-      Notificador.ExibirMensagem("Nenhum item registrado.");
-      return;
     }
 
-    Console.WriteLine(
-        "{0, -7} | {1, -10} | {2, -15} | {3, -20} | {4, -15}",
-        "Id", "Nome", "Telefone", "Cartao do Sus", "CPF"
-    );
-
-    foreach (Funcionario p in funcionarios)
+    public override void VisualizarTodos(bool deveExibirCabecalho)
     {
+      if (deveExibirCabecalho)
+        ExibirCabecalho("Visualização de Pacientes");
+
+      List<Funcionario> funcionarios = repositorio.SelecionarTodos();
+
+      if (funcionarios.Count == 0)
+      {
+        Notificador.ExibirMensagem("Nenhum item registrado.");
+        return;
+      }
+
       Console.WriteLine(
-          "{0, -7} | {1, -10} |  {3, -20} | {4, -15}",
-          p.Id, p.Nome, p.Telefone, p.Cpf
+          "{0, -7} | {1, -10} | {2, -15} | {3, -20} | {4, -15}",
+          "Id", "Nome", "Telefone", "Cartao do Sus", "CPF"
       );
+
+      foreach (Funcionario p in funcionarios)
+      {
+        Console.WriteLine(
+            "{0, -7} | {1, -10} |  {3, -20} | {4, -15}",
+            p.Id, p.Nome, p.Telefone, p.Cpf
+        );
+      }
+
+      if (deveExibirCabecalho)
+      {
+        Console.WriteLine("---------------------------------");
+        Console.Write("Digite ENTER para continuar...");
+        Console.ReadLine();
+      }
     }
 
-    if (deveExibirCabecalho)
+    protected override Funcionario ObterDadosCadastrais()
     {
-      Console.WriteLine("---------------------------------");
-      Console.Write("Digite ENTER para continuar...");
-      Console.ReadLine();
-    }
-  }
+      Console.Write("Digite o nome do Paciente: ");
+      string nome = Console.ReadLine() ?? string.Empty;
 
-  protected override Fornecedor ObterDadosCadastrais()
-  {
-    throw new NotImplementedException();
+      Console.Write("Digite o telefone do Paciente. ex: (XX) XXXX-XXXX: ");
+      string telefone = Console.ReadLine() ?? string.Empty;
+
+      Console.Write("Digite o CPF. ex: 000.000.000-00: ");
+      string cpf = Console.ReadLine() ?? string.Empty;
+
+      return new Funcionario(nome, telefone, cpf);
+    }
+
+    protected override List<string> ValidarRegistroDuplicado(Funcionario novaEntidade, string? idIgnorado = null)
+    {
+      List<string> erros = new List<string>();
+
+      List<Funcionario> funcionarios = repositorio.SelecionarTodos();
+
+      foreach (Funcionario p in funcionarios)
+      {
+        if (p.Id != idIgnorado && p.Cpf == novaEntidade.Cpf)
+        {
+          erros.Add($"Já existe um paciente com o Cartao do Sus \"{novaEntidade.Cpf}\"");
+          break;
+        }
+
+      }
+      return erros;
+    }
   }
 }
--- ControleDeMedicamentos.ConsoleApp/Utilidades/TelaPrincipal.cs
+++ ControleDeMedicamentos.ConsoleApp/Utilidades/TelaPrincipal.cs
@@ -1,22 +1,29 @@
 using ControleDeMedicamentos.ConsoleApp.Compartilhado;
 using ControleDeMedicamentos.ConsoleApp.ModuloPacientes;
 using ControleDeMedicamentos.ConsoleApp.ModuloFornecedores;
+using ControleDeMedicamentos.ConsoleApp.ModuloFuncionarios;
+
 
 namespace ControleDeMedicamentos.ConsoleApp.Utilidades;
 
 public class TelaPrincipal
 {
-  private readonly IRepositorio<Fornecedor> repositorioFornecedor;
+    private readonly IRepositorio<Fornecedor> repositorioFornecedor;
 
-  private readonly IRepositorio<Paciente> repositorioPaciente;
+    private readonly IRepositorio<Paciente> repositorioPaciente;
 
-  public TelaPrincipal(IRepositorio<Paciente> repositorioPaciente, IRepositorio<Fornecedor> repositorioFornecedor)
-  {
-    this.repositorioPaciente = repositorioPaciente;
-    this.repositorioFornecedor = repositorioFornecedor;
-  }
+    private readonly IRepositorio<Funcionario> repositorioFuncionario;
 
-  public ITelaOpcoes? ApresentarMenuOpcoesPrincipal()
+    public TelaPrincipal(IRepositorio<Paciente> repositorioPaciente,
+     IRepositorio<Fornecedor> repositorioFornecedor,
+    IRepositorio<Funcionario> repositorioFuncionario)
+    {
+        this.repositorioPaciente = repositorioPaciente;
+        this.repositorioFornecedor = repositorioFornecedor;
+        this.repositorioFuncionario = repositorioFuncionario;
+    }
+
+    public ITelaOpcoes? ApresentarMenuOpcoesPrincipal()
     {
         Console.Clear();
         Console.WriteLine("---------------------------------");
@@ -24,16 +31,20 @@ public class TelaPrincipal
         Console.WriteLine("---------------------------------");
         Console.WriteLine("1 - Gerenciar Pacientes");
         Console.WriteLine("2 - Gestão de Fornecedores");
+        Console.WriteLine("3 - Gestão de Funcionarios");
         Console.WriteLine("S - Sair");
         Console.WriteLine("---------------------------------");
         Console.Write("> ");
         string? opcaoMenuPrincipal = Console.ReadLine()?.ToUpper();
 
         if (opcaoMenuPrincipal == "1")
-            return new TelaPaciente(repositorioPaciente);              
+            return new TelaPaciente(repositorioPaciente);
 
         if (opcaoMenuPrincipal == "2")
             return new TelaFornecedor(repositorioFornecedor);
+
+        if (opcaoMenuPrincipal == "3")
+            return new TelaFuncionario(repositorioFuncionario);
 
         return null;
     }
