8eb38e383611914471c93b88483fc518c9e91bb2
Marco-Oliver <marcooliver2@yahoo.com>
2026-05-11 01:31:26 -0300

    commit temporario

--- ControleDeMedicamentos.ConsoleApp/ModuloFuncionarios/TelaFuncionario.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloFuncionarios/TelaFuncionario.cs
@@ -1,20 +1,51 @@
 using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.Utilidades;
+
 
 namespace ControleDeMedicamentos.ConsoleApp.ModuloFornecedores;
 
 public class TelaFuncionario : TelaBase<Fornecedor>, ITelaOpcoes, ITelaCrud
 {
-    public TelaFuncionario(IRepositorio<Fornecedor> repositorio) : base("Funcionario", repositorio)
+  public TelaFuncionario(IRepositorio<Fornecedor> repositorio) : base("Funcionario", repositorio)
+  {
+  }
+
+  public override void VisualizarTodos(bool deveExibirCabecalho)
+  {
+    if (deveExibirCabecalho)
+      ExibirCabecalho("Visualização de Pacientes");
+
+    List<Funcionario> funcionarios = repositorio.SelecionarTodos(); //repositorio.SelecionarTodos();
+
+    if (funcionarios.Count == 0)
     {
+      Notificador.ExibirMensagem("Nenhum item registrado.");
+      return;
     }
 
-    public override void VisualizarTodos(bool deveExibirCabecalho)
+    Console.WriteLine(
+        "{0, -7} | {1, -10} | {2, -15} | {3, -20} | {4, -15}",
+        "Id", "Nome", "Telefone", "Cartao do Sus", "CPF"
+    );
+
+    foreach (Funcionario p in funcionarios)
     {
-        throw new NotImplementedException();
+      Console.WriteLine(
+          "{0, -7} | {1, -10} |  {3, -20} | {4, -15}",
+          p.Id, p.Nome, p.Telefone, p.Cpf
+      );
     }
 
-    protected override Fornecedor ObterDadosCadastrais()
+    if (deveExibirCabecalho)
     {
-        throw new NotImplementedException();
+      Console.WriteLine("---------------------------------");
+      Console.Write("Digite ENTER para continuar...");
+      Console.ReadLine();
     }
+  }
+
+  protected override Fornecedor ObterDadosCadastrais()
+  {
+    throw new NotImplementedException();
+  }
 }
