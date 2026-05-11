ea6cb578e8045515801ce39e9c467593c27a8ebe
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-05-11 00:55:40 -0300

    chore: reset classes to initial state and move changes to correct branch

--- ControleDeMedicamentos.ConsoleApp/ModuloFornecedores/RepositorioFornecedorEmMemoria.cs
+++
@@ -1,8 +0,0 @@
-using ControleDeMedicamentos.ConsoleApp.Compartilhado;
-using ControleDeMedicamentos.ConsoleApp.Compartilhado.Memoria;
-
-namespace ControleDeMedicamentos.ConsoleApp.ModuloFornecedores;
-
-public class RepositorioFornecedorEmMemoria : RepositorioBaseEmMemoria<Fornecedor>, IRepositorio<Fornecedor>
-{
-}
--- ControleDeMedicamentos.ConsoleApp/ModuloFornecedores/TelaFornecedor.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloFornecedores/TelaFornecedor.cs
@@ -10,75 +10,11 @@ public class TelaFornecedor : TelaBase<Fornecedor>, ITelaOpcoes, ITelaCrud
 
     public override void VisualizarTodos(bool deveExibirCabecalho)
     {
-        if (deveExibirCabecalho)
-            ExibirCabecalho("Visualização de Fornecedores");
-
-        List<Fornecedor> fornecedores = repositorio.SelecionarTodos();
-
-        if (fornecedores.Count == 0)
-        {
-            Console.WriteLine("Nenhum fornecedor cadastrado.");
-            return;
-        }
-
-        Console.WriteLine("{0,-7} | {1,-30} | {2,-20} | {3,-18}",
-            "ID", "Nome", "Telefone", "CNPJ");
-        Console.WriteLine(new string('-', 85));
-
-        foreach (Fornecedor fornecedor in fornecedores)
-        {
-            Console.WriteLine("{0,-7} | {1,-30} | {2,-20} | {3,-18}",
-                fornecedor.Id,
-                fornecedor.Nome,
-                fornecedor.Telefone,
-                fornecedor.Cnpj);
-        }
-
-        if (deveExibirCabecalho)
-        {
-            Console.WriteLine();
-            Console.Write("Digite ENTER para continuar...");
-            Console.ReadLine();
-        }
+        throw new NotImplementedException();
     }
 
     protected override Fornecedor ObterDadosCadastrais()
     {
-        Console.Write("Nome: ");
-        string nome = Console.ReadLine() ?? string.Empty;
-
-        Console.Write("Telefone: ");
-        string telefone = Console.ReadLine() ?? string.Empty;
-
-        Console.Write("CNPJ: ");
-        string cnpj = Console.ReadLine() ?? string.Empty;
-
-        return new Fornecedor
-        {
-            Nome = nome,
-            Telefone = telefone,
-            Cnpj = cnpj
-        };
-    }
-
-    protected override List<string> ValidarRegistroDuplicado(Fornecedor novaEntidade, string? idIgnorado = null)
-    {
-        List<string> erros = new List<string>();
-
-        string cnpjNovo = novaEntidade.CnpjSemFormatacao();
-
-        foreach (Fornecedor fornecedor in repositorio.SelecionarTodos())
-        {
-            if (idIgnorado != null && fornecedor.Id == idIgnorado)
-                continue;
-
-            if (fornecedor.CnpjSemFormatacao() == cnpjNovo)
-            {
-                erros.Add($"Já existe um fornecedor cadastrado com o CNPJ '{novaEntidade.Cnpj}'.");
-                break;
-            }
-        }
-
-        return erros;
+        throw new NotImplementedException();
     }
 }
