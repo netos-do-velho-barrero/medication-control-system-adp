966070578b6e6a25b4e72dc2239fdaf685f926d6
Marco-Oliver <marcooliver2@yahoo.com>
2026-05-11 01:55:02 -0300

    refactor(ui): update main menu

--- ControleDeMedicamentos.ConsoleApp/Utilidades/TelaPrincipal.cs
+++ ControleDeMedicamentos.ConsoleApp/Utilidades/TelaPrincipal.cs
@@ -29,22 +29,32 @@ public class TelaPrincipal
         Console.WriteLine("---------------------------------");
         Console.WriteLine("Controle de Medicamentos");
         Console.WriteLine("---------------------------------");
-        Console.WriteLine("1 - Gerenciar Pacientes");
-        Console.WriteLine("2 - Gestão de Fornecedores");
-        Console.WriteLine("3 - Gestão de Funcionarios");
+        Console.WriteLine("1 - Gestão de Fornecedores");
+        Console.WriteLine("2 - Gestão de Pacientes");
+        Console.WriteLine("3 - Gestão de Medicamentos");
+        Console.WriteLine("4 - Gestão de Funcionarios");
+        Console.WriteLine("5 - Gestão de Estoque");
         Console.WriteLine("S - Sair");
         Console.WriteLine("---------------------------------");
         Console.Write("> ");
         string? opcaoMenuPrincipal = Console.ReadLine()?.ToUpper();
 
-        if (opcaoMenuPrincipal == "1")
-            return new TelaPaciente(repositorioPaciente);
 
-        if (opcaoMenuPrincipal == "2")
+        if (opcaoMenuPrincipal == "1")
             return new TelaFornecedor(repositorioFornecedor);
 
-        if (opcaoMenuPrincipal == "3")
+        if (opcaoMenuPrincipal == "2")
+            return new TelaPaciente(repositorioPaciente);
+
+        //if (opcaoMenuPrincipal == "3")
+           // return new TelaMedicamentos(repositorioMedicamentos);
+
+        if (opcaoMenuPrincipal == "4")
             return new TelaFuncionario(repositorioFuncionario);
+
+         //if (opcaoMenuPrincipal == "5")
+           // return new TelaEstoque(repositorioEstoque);
+           
 
         return null;
     }
